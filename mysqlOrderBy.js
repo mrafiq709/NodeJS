997384e6b836269c0d8265b8012a69e0d0135a29
mrafiq709 <mrafiq709@gmail.com>
2019-10-23 21:20:36 +0700

    Order by

---
+++ mysqlOrderBy.js
@@ -0,0 +1,17 @@
+// npm install mysql
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers ORDER BY name", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
