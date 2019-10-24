fcce629e654ee4d00a72e585bd4cc1bd036ffac1
mrafiq709 <mrafiq709@gmail.com>
2019-10-24 20:11:09 +0700

    db create

---
+++ createDb.js
@@ -0,0 +1,10 @@
+// npm install mongodb
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
